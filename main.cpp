cd6c57eb903da6b98019787cd8c324d80485f398
Ivan <ivan.puhachov@umontreal.ca>
2019-10-21 14:44:36 -0400

    104: setting colors

--- main.cpp
+++ main.cpp
@@ -3,14 +3,20 @@
 int main(int argc, char *argv[]) {
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
+    Eigen::VectorXd Z;
+    Eigen::MatrixXd C;
 
-    igl::readOFF("../data/cube.off", V,F);
+    igl::readOFF("../data/screwdriver.off", V,F);
+    Z = V.col(2);
+    igl::jet(Z,true,C);
     std::cout<< "Vertices: " << std::endl << V << std::endl;
     std::cout<< "Faces: " << std::endl << F << std::endl;
 
+
 //    Plot the mesh
     igl::opengl::glfw::Viewer viewer;
-    viewer.data().set_mesh(V,F);
-    viewer.data().set_face_based(true);
+    viewer.data().set_mesh(V,F); // copies the mesh into viewer
+    viewer.data().set_face_based(true); // Change the visualization mode, invalidating the cache if necessary
+    viewer.data().set_colors(C);
     viewer.launch();
 }
