17884bb7decdb0d16115015bfafde4e5d16fb786
Ivan <ivan.puhachov@umontreal.ca>
2019-10-21 15:58:47 -0400

    201: normals (3 types of approximation normals)

--- main.cpp
+++ main.cpp
@@ -1,62 +1,55 @@
 #include <igl/opengl/glfw/Viewer.h>
 //#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/per_face_normals.h>
+#include <igl/per_corner_normals.h>
+
+Eigen:: MatrixXd V;
+Eigen::MatrixXi F;
+
+Eigen::MatrixXd N_vertices;
+Eigen::MatrixXd N_faces;
+Eigen::MatrixXd N_corners;
+
+bool on_key_down(igl::opengl::glfw::Viewer& viewer, char key, int modifier)
+{
+    switch(key) {
+        case '1':
+            viewer.data().set_normals(N_faces);
+            return true;
+        case '2':
+            viewer.data().set_normals(N_vertices);
+            return true;
+        case '3':
+            viewer.data().set_normals(N_corners);
+            return true;
+        default:
+            break;
+    }
+    return false;
+    }
+
 
 int main(int argc, char *argv[]) {
-    Eigen::MatrixXd V;
-    Eigen::MatrixXi F;
-    Eigen::VectorXd Z;
-    Eigen::MatrixXd C;
-
-    igl::readOFF("../data/cow.off", V,F);
-    Z = V.col(2);
-    igl::jet(Z,true,C);
+    igl::readOFF("../data/fandisk.off", V,F);
     std::cout<< "Vertices: " << std::endl << V.size() << std::endl;
     std::cout<< "Faces: " << std::endl << F.size() << std::endl;
 
-    Eigen::Vector3d m = V.colwise().minCoeff();
-    Eigen::Vector3d M = V.colwise().maxCoeff();
-
-   Eigen::MatrixXd V_box(8,3);
-   V_box <<
-   m(0), m(1), m(2),
-   M(0), m(1), m(2),
-   M(0), M(1), m(2),
-   m(0), M(1), m(2),
-   m(0), m(1), M(2),
-   M(0), m(1), M(2),
-   M(0), M(1), M(2),
-   m(0), M(1), M(2);
-
-    Eigen::MatrixXi E_box(12,2);
-    E_box <<
-    0, 1,
-    1, 2,
-    2, 3,
-    3, 0,
-    4, 5,
-    5, 6,
-    6, 7,
-    7, 4,
-    0, 4,
-    1, 5,
-    2, 6,
-    7 ,3;
+    igl::per_vertex_normals(V,F,N_vertices);
+    igl::per_face_normals(V,F,N_faces);
+    igl::per_corner_normals(V,F,20,N_corners); //corner_threshold - threshold in degrees on sharp angles
 
 //    Plot the mesh
     igl::opengl::glfw::Viewer viewer;
+    viewer.callback_key_down = &on_key_down;
+    viewer.data().show_lines = false;
+
     viewer.data().set_mesh(V,F); // copies the mesh into viewer
+    viewer.data().set_normals(N_faces);
     viewer.data().set_face_based(true); // Change the visualization mode, invalidating the cache if necessary
-    viewer.data().set_colors(C);
-
-    viewer.data().add_points(V_box, Eigen::RowVector3d(0,1,1));
-    for (int i=0; i<E_box.rows(); i++)
-    {
-        viewer.data().add_edges(
-                V_box.row(E_box(i,0)),
-                V_box.row(E_box(i,1)),
-                Eigen::RowVector3d(0,0,1)
-                );
-    }
-
+    std::cout<<
+             "Press '1' for per-face normals."<<std::endl<<
+             "Press '2' for per-vertex normals."<<std::endl<<
+             "Press '3' for per-corner normals."<<std::endl;
     viewer.launch();
 }
